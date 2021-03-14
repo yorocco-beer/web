be0cde29ef2ff3c9d21a2607049b657fa9b234ea
masa <masa@s2factory.co.jp>
2021-03-14 20:30:52 +0900

    Add: ➕ テスト

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/css/app.pcss'],
+  css: ['@/assets/css/app.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
