c27699982f8bf5610ec16b3a3f8a0be61659e455
masa <masa@s2factory.co.jp>
2021-03-22 01:11:26 +0900

    Add: ➕ こうか

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,6 +52,16 @@ export default {
       {
         rel: 'stylesheet',
         href:
+          'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;800&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href:
           'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
       },
     ],
