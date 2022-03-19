7a8f66f2e4eeff1f057a86b7c5c3f27662074bb7
masa <masa@s2factory.co.jp>
2022-03-19 20:46:36 +0900

    Add: ➕ vol.12

--- pages/mood/index.vue
+++ pages/mood/index.vue
@@ -26,6 +26,21 @@
       <p class="divider">・</p>
 
       <div class="c-mood-thumbnail">
+        <NuxtLink to="/mood/vol12">
+          <h1>March.2022</h1>
+          <p class="by">Dishes : Ikuyo Segi, Picture : Yumi Saito</p>
+          <img
+            width="600"
+            v-lazy="require('@/assets/img/mood/vol12.jpg')"
+            class="img-fluid center"
+            alt=""
+          />
+        </NuxtLink>
+      </div>
+
+      <p class="divider">・</p>
+
+      <div class="c-mood-thumbnail">
         <NuxtLink to="/mood/vol11">
           <h1>February.2022</h1>
           <p class="by">Dishes : Ikuyo Segi, Picture : Yumi Saito</p>
--- pages/mood/vol12.vue
+++ pages/mood/vol12.vue
@@ -13,7 +13,7 @@
                 <li class="breadcrumb-item">
                   <NuxtLink to="/mood">Mood</NuxtLink>
                 </li>
-                <li class="breadcrumb-item active">vol.11</li>
+                <li class="breadcrumb-item active">vol.12</li>
               </ul>
               <!-- breadcrumb-list end -->
             </div>
@@ -27,7 +27,7 @@
         <img
           width="1480"
           height="987"
-          v-lazy="require('@/assets/img/mood/vol11.jpg')"
+          v-lazy="require('@/assets/img/mood/vol12.jpg')"
           class="img-fluid center"
           alt=""
         />
@@ -37,45 +37,37 @@
           <div class="col-md-8 col-sm-12">
             <div class="c-mood-content text-center">
               <header class="c-mood-header">
-                <h1>サーモンのムニエル マッシュポテト添え</h1>
-                <h2>Salmon Meuniere with Mashed Potato</h2>
+                <h1>ローストポーク 蕗の薹とナッツのソース</h1>
+                <h2>Roast Poak / Japanese Butterbur scape and nuts sauce</h2>
               </header>
               <div class="c-mood-text">
                 <p class="divider">・</p>
                 <p class="text with-divider">
-                  サーモンに軽く塩をし10分程おく。<br />
-                  余分な水分をペーパーでしっかりとる。<br />
-                  おろしたニンニクと少量のオリーブオイルでマリネする。<br />
-                  粉をまんべんなく魚にまぶす。 余分な粉ははたく。<br />
-                  フライパンにオイルをいれ火にかける。魚を皮目から焼く。<br />
-                  皮がカリッとなったら身を順番にやく。<br />
-                  バターを入れ溶かしながら焼き上げる。
-                </p>
-
-                <p class="text with-divider">
-                  マッシュポテトをつくる。<br />
-                  ジャガイモはしっかり洗い蒸し上げる。<br />
-                  温かいうちに皮を剥き鍋に入れる。<br />
-                  別鍋に牛乳とバターを入れ温めておく。<br />
-                  ジャガイモを崩し、温めた牛乳を入れてに火にかける。<br />
-                  滑らかになるまで火をいれる。<br />
-                  塩胡椒で味をし、少しのおろしたニンニクをいれる。<br />
-                  仕上げにシナモンを入れる。
+                  豚もも肉塊に塩、マーマレードをぬり一晩マリネする。<br />
+                  焼く前に常温に30分ほど戻して200度のオーブンで20〜30分焼く。オーブンから出したらアルミを被せ余熱で中まで火を通す。<br />
+                  ソースを作る。<br />
+                  蕗の薹はよく洗い、ざく切り。<br />
+                  にんにく生姜のみじん切り、スパイスをオリーブオイルで香りが出るまで熱し、蕗の薹を入れ炒める。<br />
+                  ボールにみじん切りの玉ねぎ、砕いたナッツ(今回はカシューナッツ)、炒めた蕗の薹、フレンチマスタードを入れ混ぜる。塩で味を整える。
                 </p>
 
                 <header class="c-mood-header with-divider">
-                  <h1>ルッコラとナッツのペースト</h1>
-                  <h2>Rocket Green Sauce</h2>
+                  <h1>若芽と菜の花、春キャベツのコールスロー</h1>
+                  <h2>Coleslaw with Wakame seaweed</h2>
                 </header>
                 <p class="divider">・</p>
                 <p class="text with-divider">
-                  カシューナッツをフードプロセッサーで砕く。<br />
-                  そこにルッコラ、パルミジャーノ、塩、胡椒、オリーブオイルを入れて撹拌する。<br />
-                  ムニエルやパンに添える。
+                  生若芽を一口大に切りる。にんにく生姜のみじん切り、スパイス、オリーブオイルを熱し香りをだす。生若芽を炒める。若布が弾く場合があるので注意。塩、きび砂糖、醤油、バルサミコ酢で軽く味付け。<br />
+                  キャベツは千切りにして塩をまぶす。<br />
+                  水気がでたら固く絞る。<br />
+                  菜の花はさっと湯掻き水気を絞って、一口大にきる。<br />
+                  玉葱をみじん切りにし水に晒す。10分ほとで水気を固く絞る。<br />
+                  全てをボールに入れ、ヨーグルト、粒マスタードを入れ和える。<br />
+                  塩ときび砂糖で味を整える。
                 </p>
 
                 <p class="divider">・</p>
-                <p class="date">2022.02.18</p>
+                <p class="date">2022.03.19</p>
                 <p class="by">
                   Dishes : Ikuyo Segi - オイチイチ -
                   <br />
@@ -96,18 +88,18 @@ import { defineComponent } from '@vue/composition-api'
 export default defineComponent({
   head: {
     title:
-      'ヨロッコビール / Yorocco Beer : Mood : vol.11 サーモンのムニエル マッシュポテト添え',
+      'ヨロッコビール / Yorocco Beer : Mood : vol.12 ローストポーク 蕗の薹とナッツのソース',
     meta: [
       {
         hid: 'og:url',
         property: 'og:url',
-        content: `${process.env.BASE_URL}/mood/vol11/`,
+        content: `${process.env.BASE_URL}/mood/vol12/`,
       },
       {
         hid: 'og:title',
         property: 'og:title',
         content:
-          'ヨロッコビール / Yorocco Beer : Mood : vol.11 サーモンのムニエル マッシュポテト添え',
+          'ヨロッコビール / Yorocco Beer : Mood : vol.12 ローストポーク 蕗の薹とナッツのソース',
       },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       {
@@ -119,7 +111,7 @@ export default defineComponent({
       {
         hid: 'og:image',
         property: 'og:image',
-        content: `${process.env.BASE_URL}/ogp/mood/vol11.jpg`,
+        content: `${process.env.BASE_URL}/ogp/mood/vol12.jpg`,
       },
       {
         hid: 'twitter:card',
