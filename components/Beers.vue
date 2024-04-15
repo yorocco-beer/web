2e48fa11774f20c321d0ae63c9b41c7dc6e353d9
masa <masa@s2factory.co.jp>
2024-04-15 17:25:10 +0900

    Add: ➕ ybap 13

--- components/Beers.vue
+++ components/Beers.vue
@@ -3,333 +3,132 @@
     <div class="container">
       <div class="tab-content">
         <div class="tab-pane fade active show" id="tab_list_01">
-          <!-- product-slider-active -->
           <div class="row">
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
+            <!-- 商品アイテムのループ -->
+            <div
+              v-for="(product, index) in products"
+              :key="index"
+              class="col-lg-3 col-md-4 col-6"
+            >
               <div class="single-product-item text-center">
                 <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_blueberry_saison/"
-                    class="product-thumbnail"
-                  >
+                  <!-- 商品リンク -->
+                  <NuxtLink :to="product.link" class="product-thumbnail">
+                    <!-- 商品画像 -->
                     <img
-                      v-lazy="require('@/assets/img/beer/thumb1.jpg')"
-                      alt=""
+                      v-lazy="require(`@/assets/img/beer/${product.image}`)"
+                      :alt="product.name"
                       width="263"
                       height="245"
                     />
                   </NuxtLink>
                 </div>
                 <div class="product-content">
+                  <!-- 商品タイトル -->
                   <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_blueberry_saison/"
-                      ><span class="sub">Y/B/A/P #001</span>
-                      Blueberry Saison
+                    <NuxtLink :to="product.link">
+                      <span class="sub">{{ product.code }}</span>
+                      {{ product.name }}
                     </NuxtLink>
                   </h6>
                 </div>
               </div>
-              <!-- Single Product Item End -->
             </div>
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_plum_plum_bon"
-                    class="product-thumbnail"
-                  >
-                    <img
-                      v-lazy="require('@/assets/img/beer/thumb2.jpg')"
-                      alt=""
-                      width="263"
-                      height="245"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_blueberry_saison/"
-                      ><span class="sub">Y/B/A/P #002</span>
-                      Plum Plum Bon
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-              <!-- Single Product Item End -->
-            </div>
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_dry_hopped_brett_saison"
-                    class="product-thumbnail"
-                  >
-                    <img
-                      v-lazy="require('@/assets/img/beer/thumb3.jpg')"
-                      width="263"
-                      height="245"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_dry_hopped_brett_saison"
-                      ><span class="sub">Y/B/A/P #003</span>
-                      Dry Hopped Brett Saison
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-              <!-- Single Product Item End -->
-            </div>
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_blueberry_prana/"
-                    class="product-thumbnail"
-                  >
-                    <img
-                      v-lazy="require('@/assets/img/beer/thumb4.jpg')"
-                      alt=""
-                      width="263"
-                      height="245"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_blueberry_prana/"
-                      ><span class="sub">Y/B/A/P #004</span>
-                      Blueberry Prana
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-              <!-- Single Product Item End -->
-            </div>
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_kinkan_brett_saison/"
-                    class="product-thumbnail"
-                  >
-                    <img
-                      v-lazy="require('@/assets/img/beer/thumb5.jpg')"
-                      alt=""
-                      width="263"
-                      height="245"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_kinkan_brett_saison/"
-                      ><span class="sub">Y/B/A/P #005</span>
-                      Kinkan Brett Saison
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-              <!-- Single Product Item End -->
-            </div>
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_barrel-blended-saison/"
-                    class="product-thumbnail"
-                  >
-                    <img
-                      v-lazy="require('@/assets/img/beer/thumb6.jpg')"
-                      alt=""
-                      width="263"
-                      height="245"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_barrel-blended-saison/"
-                      ><span class="sub">Y/B/A/P #006</span>
-                      Barrel Blended Saison
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-              <!-- Single Product Item End -->
-            </div>
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_blowin_through_jasmine/"
-                    class="product-thumbnail"
-                  >
-                    <img
-                      v-lazy="require('@/assets/img/beer/ybap/thumb7.png')"
-                      alt=""
-                      width="263"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_blowin_through_jasmine/"
-                      ><span class="sub">Y/B/A/P #007</span>
-                      Blowin’ through Jasmine
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-              <!-- Single Product Item End -->
-            </div>
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_barrel-aged-blackbird/"
-                    class="product-thumbnail"
-                  >
-                    <img
-                      v-lazy="require('@/assets/img/beer/ybap/thumb8.png')"
-                      alt=""
-                      width="263"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_barrel-aged-blackbird/"
-                      ><span class="sub">Y/B/A/P #008</span>
-                      Barrel Aged Blackbird Sipping
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-              <!-- Single Product Item End -->
-            </div>
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_kinkan-blonde_2022/"
-                    class="product-thumbnail"
-                  >
-                    <img
-                      v-lazy="require('@/assets/img/beer/ybap/thumb9.png')"
-                      alt=""
-                      width="263"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_kinkan-blonde_2022/"
-                      ><span class="sub">Y/B/A/P #009</span>
-                      Kinkan Blonde 2022
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-
-              <!-- Single Product Item End -->
-            </div>
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_blueberry-quad/"
-                    class="product-thumbnail"
-                  >
-                    <img
-                      v-lazy="require('@/assets/img/beer/ybap/thumb10.png')"
-                      alt=""
-                      width="263"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_blueberry-quad/"
-                      ><span class="sub">Y/B/A/P #010</span>
-                      BlueBerry Quad
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-
-              <!-- Single Product Item End -->
-            </div>
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink to="/beers/ybap_ume2021/" class="product-thumbnail">
-                    <img
-                      v-lazy="require('@/assets/img/beer/ybap/11.webp')"
-                      alt=""
-                      width="263"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_ume2021/"
-                      ><span class="sub">Y/B/A/P #011</span>
-                      UME 2021
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-              <!-- Single Product Item End -->
-            </div>
-
-            <div class="col-lg-3 col-md-4 col-6">
-              <!-- Single Product Item Start -->
-              <div class="single-product-item text-center">
-                <div class="products-images">
-                  <NuxtLink
-                    to="/beers/ybap_sumomo_2021/"
-                    class="product-thumbnail"
-                  >
-                    <img
-                      v-lazy="require('@/assets/img/beer/ybap/12.webp')"
-                      alt=""
-                      width="263"
-                    />
-                  </NuxtLink>
-                </div>
-                <div class="product-content">
-                  <h6 class="prodect-title">
-                    <NuxtLink to="/beers/ybap_sumomo_2021/"
-                      ><span class="sub">Y/B/A/P #012</span>
-                      SUMOMO 2021
-                    </NuxtLink>
-                  </h6>
-                </div>
-              </div>
-              <!-- Single Product Item End -->
-            </div>
+            <!-- 商品アイテムのループ終了 -->
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 
-export default defineComponent({})
+export default defineComponent({
+  data() {
+    return {
+      products: [
+        {
+          code: 'Y/B/A/P #013',
+          name: 'Strawberry Hope',
+          link: '/beers/ybap_strawberry_hope/',
+          image: 'ybap/13.jpg',
+        },
+        {
+          code: 'Y/B/A/P #012',
+          name: 'SUMOMO 2021',
+          link: '/beers/ybap_sumomo_2021/',
+          image: 'ybap/12.webp',
+        },
+        {
+          code: 'Y/B/A/P #011',
+          name: 'UME 2021',
+          link: '/beers/ybap_ume2021/',
+          image: 'ybap/11.webp',
+        },
+        {
+          code: 'Y/B/A/P #010',
+          name: 'BlueBerry Quad',
+          link: '/beers/ybap_blueberry-quad/',
+          image: 'ybap/thumb10.png',
+        },
+        {
+          code: 'Y/B/A/P #009',
+          name: 'Kinkan Blonde 2022',
+          link: '/beers/ybap_kinkan-blonde_2022/',
+          image: 'ybap/thumb9.png',
+        },
+        {
+          code: 'Y/B/A/P #008',
+          name: 'Barrel Aged Blackbird Sipping',
+          link: '/beers/ybap_barrel-aged-blackbird/',
+          image: 'ybap/thumb8.png',
+        },
+        {
+          code: 'Y/B/A/P #007',
+          name: 'Blowin’ through Jasmine',
+          link: '/beers/ybap_blowin_through_jasmine/',
+          image: 'ybap/thumb7.png',
+        },
+        {
+          code: 'Y/B/A/P #006',
+          name: 'Barrel Blended Saison',
+          link: '/beers/ybap_barrel-blended-saison/',
+          image: 'thumb6.jpg',
+        },
+        {
+          code: 'Y/B/A/P #005',
+          name: 'Kinkan Brett Saison',
+          link: '/beers/ybap_kinkan_brett_saison/',
+          image: 'thumb5.jpg',
+        },
+        {
+          code: 'Y/B/A/P #004',
+          name: 'Blueberry Prana',
+          link: '/beers/ybap_blueberry_prana/',
+          image: 'thumb4.jpg',
+        },
+        {
+          code: 'Y/B/A/P #003',
+          name: 'Dry Hopped Brett Saison',
+          link: '/beers/ybap_dry_hopped_brett_saison',
+          image: 'thumb3.jpg',
+        },
+        {
+          code: 'Y/B/A/P #002',
+          name: 'Plum Plum Bon',
+          link: '/beers/ybap_plum_plum_bon',
+          image: 'thumb2.jpg',
+        },
+        {
+          code: 'Y/B/A/P #001',
+          name: 'Blueberry Saison',
+          link: '/beers/ybap_blueberry_saison/',
+          image: 'thumb1.jpg',
+        },
+      ],
+    }
+  },
+})
 </script>
---
+++ pages/beers/ybap_strawberry_hope.vue
@@ -0,0 +1,140 @@
+<template>
+  <div id="main-wrapper">
+    <div class="breadcrumb-area">
+      <div class="container">
+        <div class="col-12">
+          <div class="row breadcrumb_box align-items-center">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+              <!-- breadcrumb-list start -->
+              <ul class="breadcrumb-list text-center">
+                <li class="breadcrumb-item">
+                  <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li class="breadcrumb-item">
+                  <NuxtLink to="/beers">Beers</NuxtLink>
+                </li>
+                <li class="breadcrumb-item active">
+                  Y/B/A/P #0013 Strawberry Hope
+                </li>
+              </ul>
+              <!-- breadcrumb-list end -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="single-product-wrap">
+      <div class="c-beer__mainimg">
+        <img
+          v-lazy="require('@/assets/img/beer/ybap/13.jpg')"
+          class="img-fluid center"
+          alt=""
+        />
+      </div>
+
+      <div class="c-beer__content">
+        <h1>HISTORY</h1>
+        <dl>
+          <dt>2021.12</dt>
+          <dd>仕込み。</dd>
+          <dt>2022.2</dt>
+          <dd>
+            木樽（コニャック樽）へ移送。ブレタノマイセス（野生酵母）を投入。
+          </dd>
+          <dt>2023.6</dt>
+          <dd>いちごを浸漬。290L容積の樽に、約38kgのいちごを投入。</dd>
+          <dt>2023.12</dt>
+          <dd>ボトリング。その後瓶内発酵・熟成</dd>
+        </dl>
+
+        <p class="note">
+          ※ 樽熟成（Barrel
+          Aging）で使用した木樽は、フランスの蒸留所で使用されていたもの。
+        </p>
+
+        <h1>TASTING NOTES</h1>
+        <p>
+          深い茶色の外観。ほんのりと赤みががっている。まず感じる香りは、ベリー系の柔らかな、ジャムを思わせるいちごの香り。口に含むと適度な心地よい酸味。その奥にはウッディな樽香やブレタノマイセス由来の香りなどがレイヤーとして感じられる。少し冷やし気味で飲むのがおすすめ。
+        </p>
+        <div class="product-details-content">
+          <div class="product_meta mt-30">
+            <div class="sku_wrapper item_meta">
+              <span class="label"> Style: </span>
+              Barrel Aged Belgian Quadrupel with Strawberry
+            </div>
+            <div class="posted_in item_meta">
+              <span class="label">Abv: </span>
+              10.5%
+            </div>
+          </div>
+
+          <div
+            class="
+              product_socials
+              section-space--mt_60
+              pb-20
+              section-space--mb_60
+            "
+          >
+            <span class="label">Share :</span>
+            <ul class="helendo-social-share socials-inline">
+              <li>
+                <a
+                  class="share-twitter helendo-twitter"
+                  href="#"
+                  target="_blank"
+                  ><i class="social_twitter"></i
+                ></a>
+              </li>
+              <li>
+                <a
+                  class="share-facebook helendo-facebook"
+                  href="#"
+                  target="_blank"
+                  ><i class="social_facebook"></i
+                ></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  head: {
+    title: 'ヨロッコビール / Yorocco Beer : Y/B/A/P #013 Strawberry Hope',
+    meta: [
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: `${process.env.BASE_URL}/beers/ybap_strawberry_hope/`,
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'ヨロッコビール / Yorocco Beer : Y/B/A/P #013 Strawberry Hope',
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:description', property: 'og:description', content: '' },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: `${process.env.BASE_URL}/ogp/13.jpg`,
+      },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      { hid: 'og:site_name', property: 'og:site_name', content: '' },
+      { hid: 'og:locale', property: 'og:locale', content: 'ja_JP' },
+      { hid: 'fb:app_id', property: 'fb:app_id', content: '' },
+    ],
+  },
+})
+</script>
